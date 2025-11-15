c3ac6fdc636a081846e9ebaa0b38dfa8167b1090
Sadkirti Murari das <sadkirtimuraridas@gmail.com>
2025-11-15 13:40:44 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
+  theme:{extend:{colors:{saffron:{50:'#fff7ed',100:'#ffeed6',200:'#ffd69a',300:'#ffbe5c',400:'#ff9b1f',500:'#ff7a00',600:'#e26500',700:'#b84f00',800:'#8f3a00',900:'#6b2b00'},'saffron-gold':'#F6C453'},
+  fontFamily:{heading:['Inter'],body:['Roboto']}}},plugins:[]
+};
